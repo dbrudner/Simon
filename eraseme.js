efa4873528845df3083d4e12a76605ce33ca0ed8
Dave Brudner <dbrudne1@gmail.com>
2017-12-28 21:31:54 -0500

    commit

--- eraseme.js
+++ eraseme.js
@@ -1,1 +1,10 @@
-console.log(Math.floor(Math.random() * 4))
+function theLoop (i) {
+  setTimeout(function () {
+    console.log("Cheese!");
+    if (--i) {          // If i > 0, keep going
+      theLoop(i);       // Call the loop again, and pass it the current value of i
+    }
+  }, 200);
+};
+
+theLoop(5)
--- script.js
+++ script.js
@@ -1,50 +1,98 @@
 $(document).ready(function() {
+	
 	var count = 0;
+	var sequence = [];
+	var playerAnswer = [];
+	var playerTurn
+
+	var green = "green"
+	var yellow = "yellow"
+	var blue = "blue"
+	var red = "red"
 
 	var startGame = () => {
-		var sequence = []
-
-		var green = "green"
-		var yellow = "yellow"
-		var blue = "blue"
-		var red = "red"
-
-		var addColorEvent = () => {
-			var randomNumber = Math.floor(Math.random() * 4)
-	
-			if (randomNumber === 0) {
-				console.log("green")
-				sequence.push(green)
-				$("#c").css("background-color", "#0AFF09")
-				setTimeout(function() {
-					$("#green").css("background-color", "darkgreen")
-				}, 2000)
-			}
-			if (randomNumber === 1) {
-				sequence.push(yellow)
-				$("#yellow").text("background-color", "#FEFF09")
-				setTimeout(function() {
-					$("#yellow").css("background-color", "#B0A64A")
-				}, 2000)
-			}
-			if (randomNumber === 2) {
-				sequence.push(blue)
-				$("#blue").css("background-color", "#1DA2FF")
-				setTimeout(function() {
-					$("#blue").css("background-color", "darkblue")
-				}, 2000)
-			}
-			if (randomNumber === 3) {
-				sequence.push(red)
-				$("#red").css("background-color", "red")
-				setTimeout(function() {
-					$("#red").css("background-color", "darkred")
-				}, 2000)
-			}
-		}
-		
-
+	    count = 0;
+	    sequence = [];
+	    playerAnswer = [];
+	    playerTurn
+	    addColorEvent()
 	}
+
+	function playColors (array, i) {
+			console.log(array);
+			var arr = array;
+			var i = 0;
+			delay();
+
+			function delay() {           
+				setTimeout(function () {    
+				    if (array[i] === 0) {
+					$("#green").css("background-color", "#0AFF09")
+					setTimeout(function() {
+						$("#green").css("background-color", "darkgreen")
+						}, 1000)			
+					}
+
+					if (array[i] === 1) {
+						$("#yellow").css("background-color", "yellow")
+						setTimeout(function() {
+							$("#yellow").css("background-color", "#B0A64A")
+							}, 1000)			
+					}
+
+					if (array[i] === 2) {
+						$("#blue").css("background-color", "#1DA2FF")
+						setTimeout(function() {
+							$("#blue").css("background-color", "darkblue")
+							}, 1000)			
+					}
+
+					if (array[i] === 3) {
+						$("#red").css("background-color", "red")
+						setTimeout(function() {
+							$("#red").css("background-color", "darkred")
+							}, 1000)			
+					}
+
+				    i++;                     
+				    if (i < array.length) {            
+				        delay();            
+				    }                        
+				}, 1000)
+			}		
+		};
+
+	var addColorEvent = (sequenceArray) => {
+
+		var randomNumber = Math.floor(Math.random() * 4)
+
+		// function changeColor(origianlColor, highlightColor, elementId) {
+		// 	$("elementId").css("background-color", highlightColor)
+		// 			setTimeout(function() {
+		// 				$(elementId).css("background-color", origianlColor)
+		// 				}, 1000)
+		// }
+	
+		if (randomNumber === 0) {
+			sequence.push(green)
+		}
+
+		if (randomNumber === 1) {
+			sequence.push(yellow)	
+		}
+
+		if (randomNumber === 2) {
+			sequence.push(blue)	
+		}
+		if (randomNumber === 3) {
+			sequence.push(red)
+		}
+
+		playerTurn = false;
+	}
+	
+
+	playColors([1,2,0,1], 0)
 
 	$("#start").on("click", function() {
 		startGame()
