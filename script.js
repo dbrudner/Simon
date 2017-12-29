9ae0902cb5bc0e9028842c5a7ce604eeaf2ea65e
Dave Brudner <dbrudne1@gmail.com>
2017-12-28 22:06:34 -0500

    commit

--- script.js
+++ script.js
@@ -10,12 +10,18 @@ $(document).ready(function() {
 	var blue = "blue"
 	var red = "red"
 
+	var showTurnNumber = (turnNumber) => {
+		$("#turn-count").text(`Turn: ${turnNumber}`)
+	}
+
 	var startGame = () => {
-	    count = 0;
+	    turn = 0;
 	    sequence = [];
 	    playerAnswer = [];
-	    playerTurn
-	    addColorEvent()
+	    addColor();
+	    playColors([0,1,2,3], 0);
+	    $("button").remove()
+	    // playerTurn();
 	}
 
 	function playColors (array, i) {
@@ -24,54 +30,71 @@ $(document).ready(function() {
 			var i = 0;
 			delay();
 
+			function changeColor(originalColor, highlightColor, elementId, audioLink) {
+				$(elementId).css("background-color", highlightColor)
+						new Audio(audioLink).play()
+						setTimeout(function() {
+							$(elementId).css("background-color", originalColor)
+							
+							}, 1000)
+			}
+
 			function delay() {           
 				setTimeout(function () {    
-				    if (array[i] === 0) {
-					$("#green").css("background-color", "#0AFF09")
-					setTimeout(function() {
-						$("#green").css("background-color", "darkgreen")
-						}, 1000)			
+				    if (array[i] === "green") {
+				    	changeColor("darkGreen", "#0AFF09", "#green", 'https://s3.amazonaws.com/freecodecamp/simonSound1.mp3')
+				    	
+					// $("#green").css("background-color", "#0AFF09")
+					// setTimeout(function() {
+					// 	$("#green").css("background-color", "darkgreen")
+					// 	}, 1000)			
 					}
 
-					if (array[i] === 1) {
-						$("#yellow").css("background-color", "yellow")
-						setTimeout(function() {
-							$("#yellow").css("background-color", "#B0A64A")
-							}, 1000)			
+					if (array[i] === "yellow") {
+						changeColor("#B0A64A", "yellow", "#yellow", 'https://s3.amazonaws.com/freecodecamp/simonSound2.mp3')
+
+						// $("#yellow").css("background-color", "yellow")
+						// setTimeout(function() {
+						// 	$("#yellow").css("background-color", "#B0A64A")
+						// 	}, 1000)			
 					}
 
-					if (array[i] === 2) {
-						$("#blue").css("background-color", "#1DA2FF")
-						setTimeout(function() {
-							$("#blue").css("background-color", "darkblue")
-							}, 1000)			
+					if (array[i] === "blue") {
+
+						changeColor("darkblue", "#1DA2FF", "#blue", 'https://s3.amazonaws.com/freecodecamp/simonSound3.mp3')
+						
+						// $("#blue").css("background-color", "#1DA2FF")
+						// setTimeout(function() {
+						// 	$("#blue").css("background-color", "darkblue")
+						// 	}, 1000)			
 					}
 
-					if (array[i] === 3) {
-						$("#red").css("background-color", "red")
-						setTimeout(function() {
-							$("#red").css("background-color", "darkred")
-							}, 1000)			
+					if (array[i] === "red") {
+
+						changeColor("darkred", "red", "#red", 'https://s3.amazonaws.com/freecodecamp/simonSound4.mp3')
+						
+						// $("#red").css("background-color", "red")
+						// setTimeout(function() {
+						// 	$("#red").css("background-color", "darkred")
+						// 	}, 1000)			
 					}
 
 				    i++;                     
 				    if (i < array.length) {            
 				        delay();            
-				    }                        
+				    }
+
+				    if (i === array.length) {
+				    	playerTurn = true;
+				    }
+
 				}, 1000)
 			}		
 		};
 
-	var addColorEvent = (sequenceArray) => {
+	var addColor = () => {
 
 		var randomNumber = Math.floor(Math.random() * 4)
-
-		// function changeColor(origianlColor, highlightColor, elementId) {
-		// 	$("elementId").css("background-color", highlightColor)
-		// 			setTimeout(function() {
-		// 				$(elementId).css("background-color", origianlColor)
-		// 				}, 1000)
-		// }
 	
 		if (randomNumber === 0) {
 			sequence.push(green)
@@ -88,11 +111,18 @@ $(document).ready(function() {
 			sequence.push(red)
 		}
 
+		turn++
+		console.log(turn);
+		showTurnNumber(turn)
 		playerTurn = false;
 	}
-	
 
-	playColors([1,2,0,1], 0)
+	var playerTurn = () => {
+
+
+
+	}
+	
 
 	$("#start").on("click", function() {
 		startGame()
